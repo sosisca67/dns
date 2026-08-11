991a22729f1b71a1bfea70a7f114b8a1158c76ae
sosisca67 <sosiscafree@gmail.com>
2026-08-11 14:49:09 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,20 @@
 python dns-repair.py
+python dns-repair.py
+@echo off
+chcp 1251 >nul
+cls
+
+echo DNS repair
+
+timeout /t 2 /nobreak >nul
+echo Successfully
+
+timeout /t 1 /nobreak >nul
+
+echo Roblox repair
+
+timeout /t 2 /nobreak >nul
+echo Successfully
+
+
+pause
